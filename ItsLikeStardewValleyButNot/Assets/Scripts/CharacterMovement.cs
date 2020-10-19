55e6a74c43aa004f28c7a54a4001284ef332d9fd
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2020-10-19 20:43:45 +0100

    Floor and tools
    start for floor and tools added

---
+++ ItsLikeStardewValleyButNot/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public GameObject thisObject;
+    [SerializeField] private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            thisObject.transform.position += new Vector3(0, moveSpeed + Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            thisObject.transform.position -= new Vector3(moveSpeed + Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            thisObject.transform.position -= new Vector3(0, moveSpeed + Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            thisObject.transform.position += new Vector3(moveSpeed + Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ ItsLikeStardewValleyButNot/Assets/Scripts/Tool_HoeScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool_HoeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
+            {
+                if (hit.transform.tag == ("Floor"))
+                {
+
+                }
+            }
+        }
+    }
+}
