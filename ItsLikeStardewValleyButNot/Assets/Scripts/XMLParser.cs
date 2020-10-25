894d0e30b40e40ff80708a07c5c536aba7a7b929
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2020-10-25 15:51:45 +0000

    XML and Parser added

---
+++ ItsLikeStardewValleyButNot/Assets/Scripts/XMLParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+struct itemVar
+{
+    
+}
+
+public class XMLParser : MonoBehaviour
+{
+    Dictionary<string, itemVar> items;
+
+    private void Start()
+    {
+        XmlDocument newXml = new XmlDocument();
+        newXml.Load("Assets/Scripts/XML Files/Item File.xml");
+    }
+
+    private void parseXML(XmlDocument data)
+    {
+        XmlNode root = data.DocumentElement;
+        XmlNodeList constVarList = data.SelectNodes("Items");
+        foreach (XmlNode ItemsXML in constVarList)
+        {
+            
+        }
+            
+    }
+    
+}
+
+
