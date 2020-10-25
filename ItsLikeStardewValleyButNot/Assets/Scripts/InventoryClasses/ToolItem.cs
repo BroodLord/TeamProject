ffe9edf6bcf457b759475cdb2f6754452dfb081e
Danny Kavanagh <47176624+BroodLord@users.noreply.github.com>
2020-10-25 16:15:00 +0000

    Revert Revert Revert Revert
    God Help us

---
+++ ItsLikeStardewValleyButNot/Assets/Scripts/InventoryClasses/ToolItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ToolItem : ItemBase
+{
+    public void Init(string Name, int Amount)
+    {
+        mName = Name;
+        mAmount = Amount;
+        mItemType = ItemTypes.Tool;
+    }
+
+    public void Interact()
+    {
+        Debug.Log("UWU INTERACT UWU");
+    }
+}
