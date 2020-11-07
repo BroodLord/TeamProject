bb842759366b349db6e0dcd5b07b5f2a17420459
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2020-11-07 16:16:29 +0000

    XML Parser Complete
    Parses XML document, stores all items in a dictionary that currently doesnt get used

--- ItsLikeStardewValleyButNot/Assets/Scripts/InventoryClasses/ItemBase.cs
+++ ItsLikeStardewValleyButNot/Assets/Scripts/InventoryClasses/ItemBase.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 
 public class ItemBase : MonoBehaviour
 {
-    public enum ItemTypes {Tool }
+    public enum ItemTypes { Tool, Seed, Decoration }
     protected ItemTypes mItemType;
 
     protected string mName;
     protected int mAmount;
+    protected bool mStackable;
+    protected string mSrcImage;
+    protected float mSellPrice;
 
     public string GetName() { return mName; }
     public int GetAmount() { return mAmount; }
     public ItemTypes GetType() { return mItemType; }
+
+    public bool GetStackable() { return mStackable; }
+
+    public string GetSrcImage() { return mSrcImage; }
+
+    public float GetSellPrice() { return mSellPrice; }
     public void SetName(string ItemName) { mName = ItemName; }
     public void SetAmount(int ItemAmount) { mAmount = ItemAmount; }
     public void AddAmount(int ItemAmount) { mAmount += ItemAmount; }
     public void SubstractAmount(int ItemAmount) { mAmount -= ItemAmount; }
     public void SetType(ItemTypes Type) { mItemType = Type; }
+    public void SetImage(string image) { mSrcImage = image; }
+    public void SetStackable(bool stackable) { mStackable = stackable; }
+
+    public void SetSellPrice(float sellPrice) { mSellPrice = sellPrice; }
 
 }
--- ItsLikeStardewValleyButNot/Assets/Scripts/XMLParser.cs
+++ ItsLikeStardewValleyButNot/Assets/Scripts/XMLParser.cs
@@ -4,28 +4,73 @@ using System.Xml;
 using System.Xml.Serialization;
 using UnityEngine;
 
-struct itemVar
-{
-    
-}
 
 public class XMLParser : MonoBehaviour
 {
-    Dictionary<string, itemVar> items;
+    Dictionary<string, ItemBase> items = new Dictionary<string, ItemBase>();
 
     private void Start()
     {
         XmlDocument newXml = new XmlDocument();
         newXml.Load("Assets/Scripts/XML Files/Item File.xml");
+        parseXML(newXml);
     }
 
     private void parseXML(XmlDocument data)
     {
         XmlNode root = data.DocumentElement;
-        XmlNodeList constVarList = data.SelectNodes("Items");
+
+        XmlNodeList constVarList = root.SelectNodes("Item");
+
+
         foreach (XmlNode ItemsXML in constVarList)
         {
+            ItemBase temp = new ItemBase();
+
+            temp.SetName(ItemsXML.Attributes.GetNamedItem("name").Value);
+            string itemType = ItemsXML.Attributes.GetNamedItem("type").Value;
+            string stackable = ItemsXML.Attributes.GetNamedItem("stackable").Value;
+            float sellPrice;
+
+            if (float.TryParse(ItemsXML.Attributes.GetNamedItem("sellPrice").Value, out float result))
+            {
+                sellPrice = result;
+            }
+            else
+            {
+                sellPrice = 0.0f;
+            }
             
+            if ( itemType == "Tool")
+            {
+                temp.SetType(ItemBase.ItemTypes.Tool);
+            }
+            else if ( itemType == "Seed")
+            {
+                temp.SetType(ItemBase.ItemTypes.Seed);
+            }
+            else if ( itemType == "Decoration")
+            {
+                temp.SetType(ItemBase.ItemTypes.Decoration);
+            }
+
+            temp.SetImage(ItemsXML.Attributes.GetNamedItem("src-image").Value);
+
+            if ( stackable == "Yes")
+            {
+                temp.SetStackable(true);
+            }
+            else if (stackable == "No")
+            {
+                temp.SetStackable(false);
+            }
+
+            temp.SetName(ItemsXML.Attributes.GetNamedItem("name").Value);
+
+            temp.SetSellPrice(sellPrice);
+
+            items.Add(temp.GetName(), temp);
+
         }
             
     }
