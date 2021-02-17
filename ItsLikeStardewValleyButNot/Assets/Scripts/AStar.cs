53b8f6010c2d47d8161bb71a3e8851d081194646
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2021-02-17 13:26:15 +0000

    A*
    positions for a* are a little off

--- ItsLikeStardewValleyButNot/Assets/Scripts/AStar.cs
+++ ItsLikeStardewValleyButNot/Assets/Scripts/AStar.cs
@@ -14,47 +14,56 @@ public class AStar : MonoBehaviour
     public Tilemap Nonwalkable;
     JunkPlacer placer;
 
-    private void Awake()
-    {
-        //npcPathfind(Vector3Int.RoundToInt(this.transform.position), Vector3Int.RoundToInt(goal.transform.position), Nonwalkable, grid);
-    }
     public List<Node> npcPathfind(Vector3Int Start, Vector3Int Goal, Tilemap nonWalkable, Grid grid)
     {
-        bool found = false;
-
-        
-
         List<Node> openList = new List<Node>();
         List<Node> closedList = new List<Node>();
         List<Node> result = new List<Node>();
 
         Node startNode = new Node(grid.WorldToCell(Start));
-
+        startNode.worldPosition = Start;
         Vector3Int goal = grid.WorldToCell(Goal);
+
 
         openList.Add(startNode);
         Node currentNode = openList[0];
 
-        while (openList.Count < 100 || openList.Count > 0 || !found)
+        while (openList.Count > 0)
         {
             //make closest node to the goal the current node
             currentNode = openList[0];
 
             //remove from the openlist, add to closed 
             openList.Remove(currentNode);
-            closedList.Add(currentNode);
+            
 
             //check to see if current is the goal, if so end
-            if (currentNode.worldPosition == goal)
+            if (currentNode.gridPosition == goal)
             {
-                found = true;
-                break;
+                bool foundStart = false;
+
+                do
+                {
+                    if (currentNode.parent == null)
+                    {
+                        foundStart = true;
+                    }
+                    else
+                    {
+                        result.Insert(0, currentNode);
+                        currentNode = currentNode.parent;
+                    }
+                } while (!foundStart);
+
+                result.Insert(0, startNode);
+
+                return result;
             }
 
-            Vector3Int up = new Vector3Int(currentNode.worldPosition.x, currentNode.worldPosition.y + 1, 0);
-            Vector3Int right = new Vector3Int(currentNode.worldPosition.x + 1, currentNode.worldPosition.y , 0);
-            Vector3Int down = new Vector3Int(currentNode.worldPosition.x, currentNode.worldPosition.y - 1, 0);
-            Vector3Int left = new Vector3Int(currentNode.worldPosition.x - 1, currentNode.worldPosition.y, 0);
+            Vector3Int up = new Vector3Int(currentNode.gridPosition.x, currentNode.gridPosition.y + 1, 0);
+            Vector3Int right = new Vector3Int(currentNode.gridPosition.x + 1, currentNode.gridPosition.y , 0);
+            Vector3Int down = new Vector3Int(currentNode.gridPosition.x, currentNode.gridPosition.y - 1, 0);
+            Vector3Int left = new Vector3Int(currentNode.gridPosition.x - 1, currentNode.gridPosition.y, 0);
 
             Node Temp;
 
@@ -64,10 +73,11 @@ public class AStar : MonoBehaviour
             if (nonWalkable.GetTile(up) == null)
             {
                 Temp = new Node(up);
-                if (!openList.Contains(Temp) || !closedList.Contains(Temp))
+
+                if (!searchList(openList, Temp.gridPosition.x, Temp.gridPosition.y) && !searchList(closedList, Temp.gridPosition.x, Temp.gridPosition.y))
                 {
                     Temp.parent = currentNode;
-                    //GameObject instance = Instantiate(tempObj, Temp.worldPosition, Quaternion.identity);
+                    Temp.worldPosition = grid.CellToWorld(Temp.gridPosition);
                     openList.Add(Temp);
                 }
             }
@@ -75,10 +85,10 @@ public class AStar : MonoBehaviour
             if (nonWalkable.GetTile(right) == null)
             {
                 Temp = new Node(right);
-                if (!openList.Contains(Temp) || !closedList.Contains(Temp))
+                if (!searchList(openList, Temp.gridPosition.x, Temp.gridPosition.y) && !searchList(closedList, Temp.gridPosition.x, Temp.gridPosition.y))
                 {
                     Temp.parent = currentNode;
-                    //GameObject instance = Instantiate(tempObj, Temp.worldPosition, Quaternion.identity);
+                    Temp.worldPosition = grid.CellToWorld(Temp.gridPosition);
                     openList.Add(Temp);
                 }
             }
@@ -86,10 +96,10 @@ public class AStar : MonoBehaviour
             if (nonWalkable.GetTile(down) == null)
             {
                 Temp = new Node(down);
-                if (!openList.Contains(Temp) || !closedList.Contains(Temp))
+                if (!searchList(openList, Temp.gridPosition.x, Temp.gridPosition.y) && !searchList(closedList, Temp.gridPosition.x, Temp.gridPosition.y))
                 {
                     Temp.parent = currentNode;
-                    //GameObject instance = Instantiate(tempObj, Temp.worldPosition, Quaternion.identity);
+                    Temp.worldPosition = grid.CellToWorld(Temp.gridPosition);
                     openList.Add(Temp);
                 }
             }
@@ -97,38 +107,62 @@ public class AStar : MonoBehaviour
             if (nonWalkable.GetTile(left) == null)
             {
                 Temp = new Node(left);
-                if (!openList.Contains(Temp) || !closedList.Contains(Temp))
+                if (!searchList(openList, Temp.gridPosition.x, Temp.gridPosition.y) && !searchList(closedList, Temp.gridPosition.x, Temp.gridPosition.y))
                 {
                     Temp.parent = currentNode;
-                    //GameObject instance = Instantiate(tempObj, Temp.worldPosition, Quaternion.identity);
+                    Temp.worldPosition = grid.CellToWorld(Temp.gridPosition);
                     openList.Add(Temp);
                 }
             }
 
+            closedList.Add(currentNode);
 
+            openList.Sort((x, y) => { return (goal - x.gridPosition).sqrMagnitude.CompareTo((goal - y.gridPosition).sqrMagnitude); });
 
-            openList.Sort((x, y) => { return (goal - x.worldPosition).sqrMagnitude.CompareTo((goal - y.worldPosition).sqrMagnitude); });
-
-
-
+            //Debug.Log(openList.Count);
         }
 
-        if (found)
-        {
-            while (currentNode.parent != null)
-            {
-                result.Add(currentNode.parent);
-                nonWalkable.SetTile(currentNode.parent.worldPosition, tempTile);
-                currentNode = currentNode.parent;
-            }
-            return result;
-        }
-        else
-        {
-            return null;
-        }
-       
+        Debug.Log(closedList.Count);
+        return null;
     }
+
+    bool searchList(List<Node> list, int x, int y)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i].gridPosition.x == x && list[i].gridPosition.y == y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //static void insertSort(List<Node> list)
+    //{
+    //    int n = list.Count;
+    //    int flag;
+    //
+    //    Node val;
+    //
+    //    if (list.Count > 1)
+    //    {
+    //        for (int i = 1; i < n; i++)
+    //        {
+    //            val = list[i];
+    //            flag = 0;
+    //            for (int j = i - 1; j >= 0 && flag != 1;)
+    //            {
+    //                if (val.totalCost < list[j].totalCost)
+    //                {
+    //                    list[j + 1] = list[j];
+    //                    j--;
+    //                    list[j + 1] = val;
+    //                }
+    //                else flag = 1;
+    //            }
+    //        }
+    //    }
+    //}
 }
 
 
--- ItsLikeStardewValleyButNot/Assets/Scripts/NPCMovement.cs
+++ ItsLikeStardewValleyButNot/Assets/Scripts/NPCMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
 public class NPCMovement : MonoBehaviour
 {
     private AStar pathfinding;
@@ -15,6 +16,8 @@ public class NPCMovement : MonoBehaviour
     [SerializeField]private int currentState;
     [SerializeField]private float NPCSpeed = 0.2f;
     private int pathIndex;
+    private int waypointIndex;
+    float timer = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +25,9 @@ public class NPCMovement : MonoBehaviour
         currentState = 3;
         waypointMaster = GameObject.FindGameObjectWithTag("Waypoints");
         waypoints = waypointMaster.gameObject.GetComponentsInChildren<Transform>();
-        target = waypoints[0].position;
-        nonWalkable = GameObject.FindGameObjectWithTag("NoneWalkable").GetComponent<Tilemap>();
+        waypointIndex = 1;
+        target = waypoints[waypointIndex].position;
+        nonWalkable = GameObject.FindGameObjectWithTag("Non-Walkable").GetComponent<Tilemap>();
         grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
 
     }
@@ -34,49 +38,68 @@ public class NPCMovement : MonoBehaviour
         //perform A*
         //whilst there is a target and the npc hasnt reached the target do nothing
         //if there isnt a target or the npc reaches a target, go into a waiting state (until a certain time possibly) then cycle to the next target 
-        float movement = NPCSpeed * Time.deltaTime;
-        
-        switch (currentState)
-        {
-            //Movement State
-            case 1:
-                //follow the determined path
-                if (pathIndex == currentPath.Count)
-                {
+         float movement = NPCSpeed * Time.deltaTime;
+         
+         switch (currentState)
+         {
+             //Movement State
+             case 1:
+                 //follow the determined path
+                 if (pathIndex >= currentPath.Count)
+                 {
+                    waypointIndex++;
+                    target = waypoints[waypointIndex].position;
                     currentState = 2;
-                }
-                if (Vector3.Distance(this.transform.position, currentPath[pathIndex].worldPosition) < 0.1f)
-                {
-                    pathIndex++;
-                }
-                else
-                {
-                    Vector2 v2;
-                    v2.x = currentPath[pathIndex].worldPosition.x;
-                    v2.y = currentPath[pathIndex].worldPosition.y;
-                    this.transform.position = Vector2.MoveTowards(this.transform.position, v2, movement);
-                }
-                //when the destination is reached go to 2
-                break;
-            //Waiting state
-            case 2:
+                 }
+                 else if (Vector3.Distance(this.transform.position, currentPath[pathIndex].worldPosition) < 0.01f)
+                 {
+                     pathIndex++;
+                 }
+                 else
+                 {
+                     Vector2 v2;
+                     v2.x = currentPath[pathIndex].worldPosition.x;
+                     v2.y = currentPath[pathIndex].worldPosition.y;
+                     this.transform.position = Vector2.MoveTowards(this.transform.position, v2, movement);
+                 }
+                 //when the destination is reached go to 2
+                 break;
+             //Waiting state
+             case 2:
                 //wait until the given time
+                
+                timer -= Time.deltaTime;
+                if (timer < 0)
+                {
+                    timer = 2.0f;
+                    currentState = 3;
+                }
+                 //go to 3
+                 
+                 break;
+             //Path search state
+             case 3:
+                 //find and update new path
+                 Vector3Int thisV3I = Vector3Int.FloorToInt(this.transform.position);
+                 Vector3Int targetV3I = Vector3Int.FloorToInt(target);
 
-                //go to 3
-                currentState = 3;
-                break;
-            //Path search state
-            case 3:
-                //find and update new path
-                Vector3Int thisV3I = Vector3Int.FloorToInt(this.transform.position);
-                Vector3Int targetV3I = Vector3Int.FloorToInt(target);
-                //Its probably not working because the current path needs to be followed in world space
-                currentPath = pathfinding.npcPathfind(thisV3I, targetV3I, nonWalkable, grid);
-                //reset path index
-                pathIndex = 0;
-                //go back to case 1
-                currentState = 1;
-                break;
-        }
+                 currentPath = pathfinding.npcPathfind(thisV3I, targetV3I, nonWalkable, grid);
+                if (currentPath == null)
+                {
+                    Debug.Log("shits gone tits up");
+                    Debug.Break();
+                }
+                
+                 //reset path index
+                 pathIndex = 0;
+                 //go back to case 1
+                 currentState = 1;
+                 break;
+         }
+
+        //Vector3Int thisV3I = Vector3Int.FloorToInt(this.transform.position);
+        //Vector3Int targetV3I = Vector3Int.FloorToInt(target);
+        //currentPath = pathfinding.npcPathfind(thisV3I, targetV3I, nonWalkable, grid);
+        //Debug.Log(currentPath[0].worldPosition);
     }
 }
